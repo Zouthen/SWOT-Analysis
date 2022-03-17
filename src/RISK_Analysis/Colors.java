7362c56f53b1812f3467f8de3f5e5de08c8503e9
Zouthen <mich5n69@stud.kea.dk>
2022-03-17 11:20:17 +0100

    Risk Analysis

---
+++ src/RISK_Analysis/Colors.java
@@ -0,0 +1,15 @@
+package RISK_Analysis;
+
+public class Colors {
+
+    // Reset
+    public static final String RESET = "\033[0m";
+
+    // Colors
+    public static final String RED = "\033[0;31m";
+    public static final String GREEN = "\033[0;32m";
+    public static final String YELLOW = "\033[0;33m";
+    public static final String RED_BRIGHT = "\033[0;91m";
+    public static final String GREEN_BRIGHT = "\033[0;92m";
+    public static final String WHITE_BRIGHT = "\033[0;97m";
+}
