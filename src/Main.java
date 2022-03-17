965ff0df726af8b5925a0c756374aa2ba4c55c10
Zouthen <mich5n69@stud.kea.dk>
2022-03-17 10:11:11 +0100

    Weakness

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
 public class Main {
+    public static final String ANSI_RESET = "\u001B[0m";
 
-
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static void main(String[] args) {
+        System.out.println(ANSI_RED + """
+                Weaknesses
+                1: Not much coding experience
+                2: Impatient
+                3: Small group
+                4: Very specialised group, 
+                because we all have same function (to write code)
+                5: We are students
+                """ + ANSI_RESET);
+    }
 }
